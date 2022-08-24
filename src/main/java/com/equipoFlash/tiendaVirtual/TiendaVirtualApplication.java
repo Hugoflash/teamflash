89036f0a3e266468d222b5b3422306244f4ccde3
Hugoflash <111834491+Hugoflash@users.noreply.github.com>
2022-08-23 23:17:52 -0500

    Añadir archivos de proyecto

---
+++ src/main/java/com/equipoFlash/tiendaVirtual/TiendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.equipoFlash.tiendaVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipoFlash/tiendaVirtual/TiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipoFlash.tiendaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
