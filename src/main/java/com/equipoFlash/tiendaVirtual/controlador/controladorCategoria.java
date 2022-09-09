2b76374be8212a0550cd0bf6911b9fe38de68a6e
andersonPaez <anderson.paez.1412@gmail.com>
2022-09-09 14:15:43 -0500

    ok

--- src/main/java/com/equipoFlash/tiendaVirtual/controlador/controladorCategoria.java
+++ src/main/java/com/equipoFlash/tiendaVirtual/controlador/controladorCategoria.java
@@ -1,11 +1,15 @@
 
 package com.equipoFlash.tiendaVirtual.controlador;
 
+import com.equipoFlash.tiendaVirtual.entidades.Categoria;
 import com.equipoFlash.tiendaVirtual.servicios.servicioCategoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -37,9 +41,36 @@ public class controladorCategoria
    }
    
    @GetMapping("/categoria/crearcategoria")
-   public String crearNuevaCategoria()
+   public String crearNuevaCategoria(Model modelo)
    {
+       Categoria categoria = new Categoria();
+       modelo.addAttribute("categoria",categoria);
        return "nueva_categoria";
+   } 
+   
+   @PostMapping("/categorias")
+   public String guardarCategoria(@ModelAttribute Categoria categoria)
+   {
+       servicio.guardarCategoria(categoria);
+       
+       return "redirect:/categorias";
+   }
+   
+   @GetMapping("/categoria/crearcategoria/{id}")
+   public String cargarCategoriasById(Model modelo, @PathVariable int id )
+   {
+       Categoria resultado = servicio.consultarCategoriaById(id);
+       modelo.addAttribute("categoria",resultado);
+       return "nueva_categoria";
+   }
+   
+   @GetMapping("/categoria/eliminar/{id}")
+   public String eliminarCategoria(@PathVariable int id)
+   {
+       Categoria guardado = servicio.consultarCategoriaById(id);
+       servicio.eliminarCategoria(guardado);
+       
+       return "redirect:/categorias";
    }
    
 }
